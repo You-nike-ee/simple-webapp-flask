df39001ee48f0ce9055d67e5959386a08f479e1b
Eunice <lambeeunice16@gmail.com>
2024-10-04 22:00:51 +0100

    Fix Flake8 linting errors: removed unused import and fixed blank line issues

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-import os
 from flask import Flask
+from flask_mysql import MySQL
+
 app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'eunice'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'example'
+app.config['MYSQL_DATABASE_DB'] = 'flaskdb'
+app.config['MYSQL_DATABASE_HOST'] = 'db'  # 'db' because it refers to the service name in Docker Compose
+
+mysql = MySQL()
+mysql.init_app(app)
+
 
 @app.route("/")
 def main():
     return "Welcome!"
 
+
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
